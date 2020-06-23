b53b6351d67c5a71e6c579ebc931e267ccc49e45
Marlen-Shifu <jfss7632@gmail.com>
2020-06-23 14:24:56 +0600

    second commit

--- mysite/apps/base/urls.py
+++ mysite/apps/base/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
 	path('', views.index, name = 'index'),
-	path('about-me', views.about_me, name = 'about_me'),
+	path('blog/', views.blog_page, name = 'blog_page'),
+	path('blog/<int:article_id>', views.article, name = 'article'),
 ]
--- mysite/apps/base/views.py
+++ mysite/apps/base/views.py
@@ -8,8 +8,11 @@ def index(request):
 			'author' : Article.objects.filter(id=1)[0].author}
 	return render(request, 'index.html', context=data)
 
-def about_me(request):
-	data = {'article_title' : Article.objects.filter(id=2)[0].article_title,
-			'text' : Article.objects.filter(id=2)[0].text,
-			'author' : Article.objects.filter(id=2)[0].author}
-	return render(request, 'index.html', context = data)	
+def blog_page(request):
+	data = {'Article_list': Article.objects.all()}
+	return render(request, 'index.html', data)	
+
+def article(request, article_id):
+	data = {'Article': Article.objects.filter(id= article_id)}
+
+	return render(request, 'index.html', data)
